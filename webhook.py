5b2e3288319914cb8619ab38622f137e28431b1b
sdfyj <sdfyj007@163.com>
2019-05-16 16:36:36 +0800

    first commit

---
+++ webhook.py
@@ -0,0 +1,40 @@
+from flask import Flask
+from flask import request, jsonify, abort
+import os
+import hmac
+
+app = Flask(__name__)
+
+GITHUB_SECRET_TOKEN = os.environ.get('GITHUB_SECRET')
+REPO_PATH = os.environ.get('REPO_PATH')
+HTML_PATH = os.environ.get('HTML_PATH')
+
+
+def gitpulldeployed():
+    repo = Repo(REPO_PATH)
+    origin = repo.remotes.origin
+    origin.pull('--rebase')
+
+def hugodeployed():
+    os.system('cd '+REPO_PATH)
+    os.system('cd '+REPO_PATH)
+    return
+
+@app.route('/webhook', methods=['GET', 'POST'])
+def webhook():
+    if request.method == 'POST':
+        signature = request.headers.get('X-Hub-Signature')
+        sha, signature = signature.split('=')
+        secret = str.encode(GITHUB_SECRET_TOKEN )
+        hashhex = hmac.new(secret, request.data, digestmod='sha1').hexdigest()
+        if hmac.compare_digest(hashhex, signature):
+            gitpulldeployed()
+            return jsonify({'status': 'success'}), 200
+        else:
+            return jsonify({'status': 'bad token'}), 401
+    else:
+        abort(400)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port='8001')
